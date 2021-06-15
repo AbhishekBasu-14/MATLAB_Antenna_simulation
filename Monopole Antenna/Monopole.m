0e1dd3db8166d2adf629c8844da17b55a030556e
Abhishek Basu <abhi.basu100@gmail.com>
2021-06-15 10:34:30 +0530

    Simulation script

---
+++ Monopole Antenna/Monopole.m
@@ -0,0 +1,12 @@
+clc;
+close all;
+clear all;
+m = monopole;
+m.GroundPlaneLength = 2.5;
+m.GroundPlaneWidth = 2.5;
+%figure(1);
+%pattern(m,1200e6)
+%figure(2);
+%patternElevation(m,1200e6)
+figure(1);
+show(m);
